d7d31e1c91df18953f0ba930270c29b3bd03644f
Odysseus <odysseus.levy@vantos.com>
2012-07-28 22:45:00 -0700

    first. --uly

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saeko::Application.config.secret_token = '14af30e32979e02445de13005ca005d98429b874f4db160a23cfa79aaefe7c118994bbce9838678e77a310e1df815f5f271a4c22fdc3825a614f7609afe42a19'
